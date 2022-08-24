009158be6080e579f4fbf8a97e8efd7661cb70aa
WilliamRockett <william@rockett.fr>
2022-08-24 17:04:04 +0200

    Test iOS

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
+import 'expo-dev-client';
+
+// Pre-step, call this before any NFC operations
+NfcManager.start();
 
 export default function App() {
+  async function readNdef() {
+    try {
+      console.log(await NfcManager.isSupported())
+      console.log(await NfcManager.isEnabled())
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // await NfcManager.requestTechnology();
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn('Tag found', tag);
+    } catch (ex) {
+      console.warn(ex)
+      // console.warn('Oops!', ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={readNdef}>
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
     </View>
   );
 }
